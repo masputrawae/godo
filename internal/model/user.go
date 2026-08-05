8855e4aad81953cd8601f2bb2e7d5a2a852d609c
Putra Jaya <masputrawae@users.noreply.github.com>
2026-08-05 20:34:50 +0700

    chore: structural adjustment

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,10 +1,14 @@
 package model
 
+import "time"
+
 type User struct {
-	ID       int
-	Username string
-	Password string
-	Email    string
+	ID        int
+	Username  string
+	Password  string
+	Email     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 type UserCreatePayload struct {
