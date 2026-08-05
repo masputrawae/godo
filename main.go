23a9bec51f943ac2b669673062057c31acf6964f
Putra Jaya <masputrawae@users.noreply.github.com>
2026-08-05 20:35:34 +0700

    chore: assembling the main unit

--- main.go
+++ main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"godo/internal/infra/db"
 	_ "godo/internal/infra/logger"
+	"godo/internal/repo"
 )
 
 func main() {
 	db := db.OpenSQLite("app.db")
 	defer db.Close()
+
+	// ===== REPOSITORIES =====
+	repo := repo.New(db)
+	repoUser := repo.NewUser()
+	repoTodo := repo.NewTodo()
+	repoStatus := repo.NewStatus()
+	repoPriority := repo.NewPriority()
 }
